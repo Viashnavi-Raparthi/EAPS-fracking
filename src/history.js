50d81a868e985f6b96619b605d0e60540d83782c
Viashnavi-Raparthi <vraparth@purdue.edu>
2024-04-23 12:46:04 -0400

    added speech text to history.js

--- src/history.js
+++ src/history.js
@@ -1,6 +1,28 @@
 const START_DATE = 1850
 const END_DATE = 2000
 
+const colonelSpeeches = [
+    "In the mid nineteenth century, a lot of us engineers and lot tried to figure out how we were going to get more gas",
+    "Well, ain't nothing wrong with them quite yet but we just had so many and they weren't that efficient after some point, but we knew there was oil trapped down there",
+    "You see, in 1862, one of my good friends' dad, Colonel Edward Roberts, was on the battlefield. And he said, hey, what if we just threw a torpedo down in this oil well",
+    "Well first, all of them had said he was out of his mind. But then, the torpedo detonated and shattered the surrounding rocks",
+    "Yup, look at you picking up on this!",
+    "Well it took a lot of other technology but with horizontal drilling and this fracking idea, we were able to have great sources of domestic energy",
+    "Good? Son, it was great! The US unlocked so many different reservoirs across the country. It even helped decrease the amount of CO2 in the air because we weren't going through as much coal"
+
+];
+
+const userSpeeches = [
+    "Why, what happened to your regular methods?",
+    "So what did you do?",
+    "WHAT?! That's crazy, what happened??",
+    "OHH, so is that when it was discovered that the flow of oil increased significantly at that well?",
+    "What else happened?",
+    "So just how good was natural gas production for the US at this time then?",
+    "Huh, I never knew there was such a strong history behind this idea!"
+
+];
+
 document.addEventListener('DOMContentLoaded', function() {
     const timelineLine = document.querySelector('.timeline-line');
     const dateRange = END_DATE - START_DATE; // Total number of years in the date range
