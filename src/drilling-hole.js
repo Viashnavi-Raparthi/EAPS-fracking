eb1d261b4b6b0a7233a40dd7f508bff05ccd1ecc
Viashnavi-Raparthi <vraparth@purdue.edu>
2024-04-23 13:27:44 -0400

    added speech/blurb text to drilling-hole.js for interactiveness
    I put these on the top of the file, I don't even know if this is where they need to go.

--- src/drilling-hole.js
+++ src/drilling-hole.js
@@ -4,6 +4,23 @@ import { initAssets } from './assets.ts';
 const TOP_OF_DIRT = 330;
 const HOLE_WIDTH = 15;
 
+const colonelSpeeches = [
+  "Oh, no! Trust me we’ve got a much better system worked out now. Here, how about I show you how it’s done here. Remember that each step is crucial to the success of the entire operation."
+  ];
+  
+  const userSpeeches = [
+  "Wait, but how does this whole process work? I mean we’re surely not just still throwing explosives down into a well, right",
+  "Okay!"
+  ];
+  
+  const textBlobs = [
+  "Drill a wellbore vertically into the Earth. This will go through the layers of sediment and rock until it reaches our targeted shale formation. We can also extend our wellbore horizontally and make it go through the shale reservoir to help maximize the amount of contact",
+  "Now we need to mix some fracking fluid.",
+  "Grab this bucket and add water, sand, and some proppant.",
+  "Since the fracking fluid has sand, the sand props the fractures open. The sand here serves as a proppant, allowing the hydrocarbons to flow more freely to the surface. The chemical additives in the fluid also help keep the process efficient by reducing friction and preventing bacterial growth within the wellbore"
+  ];
+  
+
 const app = new PIXI.Application();
 await app.init({ width: 800, height: 600, preference:'webgl', backgroundColor: 0xCCCCCC})
 document.body.appendChild(app.canvas);
